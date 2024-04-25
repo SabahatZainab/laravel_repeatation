39bd70bd07f1c175d8b202b4d8e6e12e1c223b17
Sabahat Zainab <sabahatzainab65@gmail.com>
2024-04-25 17:56:11 +0500

    take update

---
+++ blade-IIII/resources/views/home.blade.php
@@ -0,0 +1,12 @@
+@include('pages.header')
+    <h1>Home Page Description</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla quasi pariatur delectus consequuntur illum minus sed cum, totam, ab sunt sint nobis! Placeat quod rerum ab necessitatibus quisquam commodi consequuntur.</p>
+</article>
+<aside>
+    <ul>
+        <li><a href="/">Home</a></li>
+        <li><a href="post">Post</a></li>
+        <li><a href="about">About</a></li>
+    </ul>
+</aside>
+@include('pages.footer')
---
+++ blade-IIII/resources/views/post.blade.php
@@ -0,0 +1,6 @@
+@include('pages.header')
+<article>
+    <h1>Post Page Description</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla quasi pariatur delectus consequuntur illum minus sed cum, totam, ab sunt sint nobis! Placeat quod rerum ab necessitatibus quisquam commodi consequuntur.</p>
+</article>
+@include('pages.footer')
